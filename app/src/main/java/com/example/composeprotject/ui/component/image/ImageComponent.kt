b449203378bf7b997c5c640314df5641197b0158
Коновалова Лера <73699428+lerapenguin11@users.noreply.github.com>
2024-08-19 18:14:54 +0500

    add community image component

--- app/src/main/java/com/example/composeprotject/ui/component/image/ImageComponent.kt
+++ app/src/main/java/com/example/composeprotject/ui/component/image/ImageComponent.kt
@@ -13,8 +13,8 @@ import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
 import coil.request.ImageRequest
 import com.example.composeprotject.R
@@ -49,7 +49,27 @@ fun EventImage(
 }
 
 @Composable
-@Preview(showBackground = true)
-fun ImageTest() {
-    //EventImageBig(placeholderImage = R.drawable.ic_event_placeholder, avatarUrl = null)
+fun CommunityImage(
+    placeholderImage: Int,
+    avatarUrl: String?,
+    modifier: Modifier = Modifier
+) {
+    Box(
+        modifier = modifier
+            .size(104.dp)
+            .clip(RoundedCornerShape(MeetTheme.sizes.sizeX16))
+            .background(color = Color.Transparent)
+    ) {
+        AsyncImage(
+            model = ImageRequest.Builder(LocalContext.current)
+                .data(avatarUrl)
+                .crossfade(true)
+                .build(),
+            placeholder = painterResource(placeholderImage),
+            error = painterResource(placeholderImage),
+            contentDescription = stringResource(R.string.text_avatar_meetings),
+            contentScale = ContentScale.Crop,
+            modifier = Modifier.fillMaxSize()
+        )
+    }
 }
