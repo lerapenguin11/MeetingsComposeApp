36d3e98c2540504f1557a5da1d64d31b25d78d28
Коновалова Лера <73699428+lerapenguin11@users.noreply.github.com>
2024-08-26 18:19:58 +0500

    add view all card

--- app/src/main/java/com/example/composeprotject/ui/component/card/CardComponent.kt
+++ app/src/main/java/com/example/composeprotject/ui/component/card/CardComponent.kt
@@ -1,17 +1,23 @@
 package com.example.composeprotject.ui.component.card
 
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import com.example.composeprotject.R
+import com.example.composeprotject.model.meeting.Meeting
 import com.example.composeprotject.ui.component.button.SubscribeButton
 import com.example.composeprotject.ui.component.card.cardStyle.EventCardStyle
 import com.example.composeprotject.ui.component.card.cardStyle.EventCardStyleDefault
@@ -24,20 +30,16 @@ import com.example.composeprotject.ui.component.image.EventImage
 import com.example.composeprotject.ui.component.state.SubscribeButtonState
 import com.example.composeprotject.ui.component.text.BaseText
 import com.example.composeprotject.ui.component.utils.FlexRow
+import com.example.composeprotject.ui.component.utils.eventDate
 import com.example.composeprotject.ui.theme.MeetTheme
 
 @Composable
 fun EventCard(
-    eventId: String,
-    avatarUrl: String?,
-    eventName: String,
-    dateEvent: String,
-    locationEvent: String,
-    tags: List<String>,
+    meeting: Meeting,
     variant: EventCardVariant,
     modifier: Modifier = Modifier,
     style: EventCardStyle = EventCardStyleDefault.eventCardStyle(),
-    onClick: () -> Unit,
+    onClick: () -> Unit
 ) {
     Card(
         onClick = onClick,
@@ -47,8 +49,8 @@ fun EventCard(
         shape = RoundedCornerShape(
             topStart = MeetTheme.sizes.sizeX16,
             topEnd = MeetTheme.sizes.sizeX16,
-            bottomEnd = 0.dp,
-            bottomStart = 0.dp
+            bottomEnd = 4.dp,
+            bottomStart = 4.dp
         ),
         modifier = modifier
             .width(width = style.widthImage(variant = variant))
@@ -56,19 +58,19 @@ fun EventCard(
         EventImage(
             width = style.widthImage(variant = variant),
             height = style.heightImage(variant = variant),
-            avatarUrl = avatarUrl,
+            avatarUrl = meeting.avatarUrl,
             placeholderImage = R.drawable.ic_event_placeholder
         )
         Spacer(modifier = Modifier.height(MeetTheme.sizes.sizeX8))
         BaseText(
-            text = eventName,
+            text = meeting.title,
             textStyle = style.titleTextStyle(variant = variant),
             textColor = MeetTheme.colors.black,
             maxLines = EVENT_NAME_MAX_LINE
         )
         Spacer(modifier = Modifier.height(MeetTheme.sizes.sizeX2))
         BaseText(
-            text = "$dateEvent · $locationEvent",
+            text = "${eventDate(timestamp = meeting.startDate)} · ${meeting.shortAddress}",
             textColor = MeetTheme.colors.darkGray,
             textStyle = MeetTheme.typography.interMedium14,
             maxLines = DATE_LOCATION_EVENT_MAX_LINE
@@ -80,9 +82,9 @@ fun EventCard(
             verticalGap = 6.dp,
             alignment = Alignment.Start,
         ) {
-            repeat(tags.size) { index ->
+            repeat(meeting.categories.size) { index ->
                 Tag(
-                    text = tags[index],
+                    text = meeting.categories[index].title,
                     chipSize = ChipSize.SMALL,
                     chipColors = ChipClick.FALSE
                 )
@@ -128,6 +130,42 @@ fun CommunityCard(
             state = buttonState,
         ) {
             //TODO
+        }
+    }
+}
+
+@Composable
+fun ViewAllCard(
+    variant: EventCardVariant,
+    modifier: Modifier = Modifier,
+    style: EventCardStyle = EventCardStyleDefault.eventCardStyle(),
+    onClick: () -> Unit
+) {
+    Card(
+        onClick = onClick,
+        colors = CardDefaults.cardColors(
+            containerColor = MeetTheme.colors.secondary,
+        ),
+        shape = RoundedCornerShape(
+            topStart = MeetTheme.sizes.sizeX16,
+            topEnd = MeetTheme.sizes.sizeX16,
+            bottomEnd = MeetTheme.sizes.sizeX16,
+            bottomStart = MeetTheme.sizes.sizeX16
+        ),
+        modifier = modifier
+            .width(width = 156.dp)
+            .height(height = style.heightImage(variant = variant))
+    ) {
+        Box(
+            modifier = Modifier.fillMaxSize(),
+            contentAlignment = Alignment.Center
+        ) {
+            Text(
+                text = stringResource(R.string.text_view_all),
+                color = MeetTheme.colors.primary,
+                style = MeetTheme.typography.interMedium18,
+                textAlign = TextAlign.Center
+            )
         }
     }
 }
