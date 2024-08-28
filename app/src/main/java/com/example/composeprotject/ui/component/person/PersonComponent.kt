b0502ca6d5050c045554ac5ab44f2b7e488ec05d
Коновалова Лера <73699428+lerapenguin11@users.noreply.github.com>
2024-08-29 00:35:20 +0500

    add PersonImage component

--- app/src/main/java/com/example/composeprotject/ui/component/person/PersonComponent.kt
+++ app/src/main/java/com/example/composeprotject/ui/component/person/PersonComponent.kt
@@ -7,17 +7,25 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.zIndex
+import coil.compose.AsyncImage
+import coil.request.ImageRequest
 import com.example.composeprotject.R
 import com.example.composeprotject.ui.component.avatar.Avatar
 import com.example.composeprotject.ui.component.avatar.variant.AvatarVariant
@@ -25,6 +33,32 @@ import com.example.composeprotject.ui.component.chip.Tag
 import com.example.composeprotject.ui.component.chip.chipStyle.ChipClick
 import com.example.composeprotject.ui.component.text.BaseText
 import com.example.composeprotject.ui.theme.MeetTheme
+
+@Composable
+fun PersonImage(
+    placeholderImage: Int,
+    avatarUrl: String?,
+    modifier: Modifier = Modifier
+) {
+    Box(
+        modifier = modifier
+            .size(width = 104.dp, height = 104.dp)
+            .clip(RoundedCornerShape(MeetTheme.sizes.sizeX16))
+            .background(color = Color.Transparent)
+    ) {
+        AsyncImage(
+            model = ImageRequest.Builder(LocalContext.current)
+                .data(avatarUrl)
+                .crossfade(true)
+                .build(),
+            placeholder = painterResource(placeholderImage),
+            error = painterResource(placeholderImage),
+            contentDescription = stringResource(R.string.text_avatar_meetings),
+            contentScale = ContentScale.Crop,
+            modifier = Modifier.fillMaxSize()
+        )
+    }
+}
 
 @Composable
 fun Person(
