0d6aae54c9fd564f330aa1bba0eebb62bced905d
Коновалова Лера <73699428+lerapenguin11@users.noreply.github.com>
2024-09-02 18:45:25 +0500

    refactoring search top bar

--- app/src/main/java/com/example/composeprotject/ui/component/topBar/SearchTopBar.kt
+++ app/src/main/java/com/example/composeprotject/ui/component/topBar/SearchTopBar.kt
@@ -18,6 +18,8 @@ import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.ripple.rememberRipple
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.OutlinedTextFieldDefaults
@@ -77,135 +79,150 @@ fun SearchBar(
             errorTextColor = MeetTheme.colors.black,
             cursorColor = MeetTheme.colors.primary
         )
-    Row(
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(
-                top = 12.dp,
-                start = 16.dp,
-                end = 16.dp,
-                bottom = 12.dp
-            ),
-        verticalAlignment = Alignment.CenterVertically
+    Card(
+        colors = CardDefaults.cardColors(
+            containerColor = Color.White
+        ),
+        shape = RoundedCornerShape(
+            bottomStart = 0.dp,
+            bottomEnd = 0.dp,
+            topStart = 0.dp,
+            topEnd = 0.dp
+        ),
+        elevation = CardDefaults.elevatedCardElevation(
+            defaultElevation = MeetTheme.sizes.sizeX5
+        )
     ) {
-        BasicTextField(
-            value = searchText,
-            onValueChange = { newValue ->
-                searchText = newValue
-                onValueChange(newValue)
-            },
-            modifier = modifier
-                .weight(5f)
-                .fillMaxWidth(),
-            interactionSource = interactionSource,
-            enabled = isEnabled,
-            singleLine = singleLine,
-            textStyle = MeetTheme.typography.interMedium14,
-            keyboardOptions = KeyboardOptions(
-                keyboardType = KeyboardType.Text,
-                imeAction = ImeAction.Done
-            ),
-            keyboardActions = KeyboardActions(
-                onDone = {
-                    keyboardController?.hide()
-                    focusManager.clearFocus()
-                }
-            ),
-            cursorBrush = Brush.verticalGradient(
-                0.00f to MeetTheme.colors.primary,
-                0.05f to MeetTheme.colors.primary
-            )
-        ) {
-            OutlinedTextFieldDefaults.DecorationBox(
-                value = searchText,
-                visualTransformation = VisualTransformation.None,
-                innerTextField = it,
-                singleLine = singleLine,
-                enabled = isEnabled,
-                placeholder = {
-                    Row(verticalAlignment = Alignment.CenterVertically) {
-                        Icon(
-                            modifier = Modifier
-                                .size(size = 22.dp),
-                            tint = MeetTheme.colors.darkGray,
-                            painter = painterResource(id = CommonDrawables.ic_event_community_search),
-                            contentDescription = stringResource(id = CommonString.text_search)
-                        )
-                        Spacer(modifier = Modifier.width(6.dp))
-                        Text(
-                            text = stringResource(id = CommonString.text_search),
-                            style = MeetTheme.typography.interMedium14,
-                            color = MeetTheme.colors.darkGray
-                        )
-                    }
-                },
-                interactionSource = interactionSource,
-                contentPadding =
-                OutlinedTextFieldDefaults.contentPadding(
-                    top = 11.dp,
-                    bottom = 11.dp,
-                    start = 10.dp,
-                    end = 10.dp
-                ),
-                colors = colors,
-                container = {
-                    OutlinedTextFieldDefaults.ContainerBox(
-                        enabled = isEnabled,
-                        isError = state == InputState.ERROR,
-                        colors = colors,
-                        interactionSource = interactionSource,
-                        shape = RoundedCornerShape(MeetTheme.sizes.sizeX16)
-                    )
-                },
-                trailingIcon = {
-                    AnimatedVisibility(visible = searchText.isNotEmpty()) {
-                        Icon(
-                            modifier = Modifier
-                                .clickable {
-                                    searchText = ""
-                                },
-                            tint = MeetTheme.colors.darkGray,
-                            painter = painterResource(id = CommonDrawables.ic_event_text_clear),
-                            contentDescription = stringResource(id = CommonString.text_search)
-                        )
-                    }
-                }
-
-            )
-        }
-        Spacer(modifier = Modifier.width(MeetTheme.sizes.sizeX8))
-        Box(
+        Row(
             modifier = Modifier
-                .height(44.dp)
-                .background(color = Color.White, shape = RoundedCornerShape(10.dp))
-                .clip(RoundedCornerShape(10.dp))
-                .clickable(
-                    interactionSource = remember { MutableInteractionSource() },
-                    indication = rememberRipple(bounded = true),
-                    onClick = {
-                        //TODO
+                .fillMaxWidth()
+                .padding(
+                    top = 12.dp,
+                    start = 16.dp,
+                    end = 16.dp,
+                    bottom = 12.dp
+                ),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            BasicTextField(
+                value = searchText,
+                onValueChange = { newValue ->
+                    searchText = newValue
+                    onValueChange(newValue)
+                },
+                modifier = modifier
+                    .weight(5f)
+                    .fillMaxWidth(),
+                interactionSource = interactionSource,
+                enabled = isEnabled,
+                singleLine = singleLine,
+                textStyle = MeetTheme.typography.interMedium14,
+                keyboardOptions = KeyboardOptions(
+                    keyboardType = KeyboardType.Text,
+                    imeAction = ImeAction.Done
+                ),
+                keyboardActions = KeyboardActions(
+                    onDone = {
+                        keyboardController?.hide()
+                        focusManager.clearFocus()
                     }
                 ),
-            contentAlignment = Alignment.Center
-        ) {
-            if (searchText.isEmpty()) {
-                Box(
-                    modifier = Modifier
-                        .width(width = 39.dp)
-                        .clip(RoundedCornerShape(10.dp))
-                ) {
-                    Image(
-                        modifier = Modifier.size(width = 32.dp, height = 44.dp),
-                        painter = painterResource(id = CommonDrawables.ic_user_profile),
-                        contentDescription = null
+                cursorBrush = Brush.verticalGradient(
+                    0.00f to MeetTheme.colors.primary,
+                    0.05f to MeetTheme.colors.primary
+                )
+            ) {
+                OutlinedTextFieldDefaults.DecorationBox(
+                    value = searchText,
+                    visualTransformation = VisualTransformation.None,
+                    innerTextField = it,
+                    singleLine = singleLine,
+                    enabled = isEnabled,
+                    placeholder = {
+                        Row(verticalAlignment = Alignment.CenterVertically) {
+                            Icon(
+                                modifier = Modifier
+                                    .size(size = 22.dp),
+                                tint = MeetTheme.colors.darkGray,
+                                painter = painterResource(id = CommonDrawables.ic_event_community_search),
+                                contentDescription = stringResource(id = CommonString.text_search)
+                            )
+                            Spacer(modifier = Modifier.width(6.dp))
+                            Text(
+                                text = stringResource(id = CommonString.text_search),
+                                style = MeetTheme.typography.interMedium14,
+                                color = MeetTheme.colors.darkGray
+                            )
+                        }
+                    },
+                    interactionSource = interactionSource,
+                    contentPadding =
+                    OutlinedTextFieldDefaults.contentPadding(
+                        top = 11.dp,
+                        bottom = 11.dp,
+                        start = 10.dp,
+                        end = 10.dp
+                    ),
+                    colors = colors,
+                    container = {
+                        OutlinedTextFieldDefaults.ContainerBox(
+                            enabled = isEnabled,
+                            isError = state == InputState.ERROR,
+                            colors = colors,
+                            interactionSource = interactionSource,
+                            shape = RoundedCornerShape(MeetTheme.sizes.sizeX16)
+                        )
+                    },
+                    trailingIcon = {
+                        AnimatedVisibility(visible = searchText.isNotEmpty()) {
+                            Icon(
+                                modifier = Modifier
+                                    .clickable {
+                                        searchText = ""
+                                    },
+                                tint = MeetTheme.colors.darkGray,
+                                painter = painterResource(id = CommonDrawables.ic_event_text_clear),
+                                contentDescription = stringResource(id = CommonString.text_search)
+                            )
+                        }
+                    }
+
+                )
+            }
+            Spacer(modifier = Modifier.width(MeetTheme.sizes.sizeX8))
+            Box(
+                modifier = Modifier
+                    .height(44.dp)
+                    .background(color = Color.White, shape = RoundedCornerShape(10.dp))
+                    .clip(RoundedCornerShape(10.dp))
+                    .clickable(
+                        interactionSource = remember { MutableInteractionSource() },
+                        indication = rememberRipple(bounded = true),
+                        onClick = {
+                            //TODO
+                        }
+                    ),
+                contentAlignment = Alignment.Center
+            ) {
+                if (searchText.isEmpty()) {
+                    Box(
+                        modifier = Modifier
+                            .width(width = 39.dp)
+                            .clip(RoundedCornerShape(10.dp))
+                    ) {
+                        Image(
+                            modifier = Modifier.size(width = 32.dp, height = 44.dp),
+                            painter = painterResource(id = CommonDrawables.ic_user_profile),
+                            contentDescription = null
+                        )
+                    }
+                } else {
+                    Text(
+                        text = stringResource(CommonString.text_cancel),
+                        color = MeetTheme.colors.primary,
+                        style = MeetTheme.typography.interSemiBold14
                     )
                 }
-            } else {
-                Text(
-                    text = stringResource(CommonString.text_cancel),
-                    color = MeetTheme.colors.primary,
-                    style = MeetTheme.typography.interSemiBold14
-                )
             }
         }
     }
